7ba99765b1a68a749f61e45bc70dcd53a1e8971a
Michael <mikemikhailov2003@gmail.com>
2019-09-17 17:32:45 +0000

    Styled pages, added npm scripts

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const bodyparser = require('body-parser');
+
+const campgrounds = [
+  {
+    name: 'Salmon Creek',
+    img: 'https://unsplash.com/photos/DgSnapS1itA/download?force=true',
+  },
+  {
+    name: 'Granite Hill',
+    img: 'https://unsplash.com/photos/pSaEMIiUO84/download?force=true',
+  },
+  {
+    name: 'Mountain Goat`s Rest',
+    img: 'https://unsplash.com/photos/ebnlHkqfUHY/download?force=true',
+  },
+];
+
+app.use(bodyparser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -9,21 +28,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-  let campgrounds = [
-    {
-      name: 'Salmon Creek',
-      img: 'https://unsplash.com/photos/8f_VQ3EFbTg/download?force=true',
-    },
-    {
-      name: 'Granite Hill',
-      img: 'https://unsplash.com/photos/pSaEMIiUO84/download?force=true',
-    },
-    {
-      name: 'Mountain Goat`s Rest',
-      img: 'https://unsplash.com/photos/ebnlHkqfUHY/download?force=true',
-    },
-  ];
-  res.render('campgrounds')
+  res.render('campgrounds', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/campgrounds', (req, res) => {
+  const { name } = req.body;
+  const { img } = req.body;
+  const newCampground = { name, img };
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
